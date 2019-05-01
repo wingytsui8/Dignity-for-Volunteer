7d8f783ad39138758553039e05bdbf20e26e75ed
wingytsui8 <tsuiwingyan@gmail.com>
2019-05-01 19:08:59 +0800

    redinent

--- public_html/view/2.php
+++ public_html/view/2.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<title>About</title>
@@ -14,7 +15,7 @@
 	<meta property="og:type" content="article" />
 	<meta property="og:url" content="{{curr_url}}" />
 	<!-- Facebook Open Graph end -->
-	
+
 	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 	<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
 	<script src="js/bootstrap.min.js" type="text/javascript"></script>
@@ -28,57 +29,109 @@
 	<script type="text/javascript">
 		window.useTrailingSlashes = true;
 	</script>
-	
-	<link href="css/flag-icon-css/css/flag-icon.min.css" rel="stylesheet" type="text/css" />	
-	<!--[if lt IE 9]>
+
+	<link href="css/flag-icon-css/css/flag-icon.min.css" rel="stylesheet" type="text/css" />
+    <!--[if lt IE 9]>
 	<script src="js/html5shiv.min.js"></script>
 <![endif]-->
 
 </head>
 
+<body>
+	<div class="root">
+		<div class="vbox wb_container" id="wb_header">
 
-<body><div class="root"><div class="vbox wb_container" id="wb_header">
-	
-	<div class="wb_cont_inner"><div id="wb_element_instance15" class="wb_element wb-menu"><ul class="hmenu"><li><a href="" target="_self">Home</a></li><li><a href="Event/" target="_self">Event</a></li><li class="active"><a href="About/" target="_self">About</a></li><li><a href="Login/" target="_self">Login</a></li></ul><div class="clearfix"></div></div><div id="wb_element_instance16" class="wb_element wb_element_picture" title=""><a href="http://dignityforchildren.org/" target="1"><img alt="gallery/dignity_logo" src="gallery_gen/37c944c27b869908c211dea96575621f_190x60.png"></a></div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div>
-	<div class="vbox wb_container" id="wb_main">
-		
-		<div class="wb_cont_inner"><div id="wb_element_instance18" class="wb_element wb_text_element" style=" line-height: normal;"><p class="wb-stl-normal">You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the contact form on our website. We wish you a good day! You will find the latest information about us on this page. Our company is constantly evolving and...</p></div><div id="wb_element_instance19" class="wb_element wb_text_element" style=" line-height: normal;"><h2 class="wb-stl-heading2">You will find the latest information...<span style="background-color: transparent;">About our company here. You will find the...</span><span style="background-color: transparent;">Latest information about our company...</span><span style="background-color: transparent;">Here. You will find the latest...</span>Information about our...</h2></div><div id="wb_element_instance20" class="wb_element wb_element_shape"><div class="wb_shp"></div></div><div id="wb_element_instance21" class="wb_element wb_text_element" style=" line-height: normal;"><h1 class="wb-stl-heading1">About us</h1></div><div id="wb_element_instance22" class="wb_element wb_text_element" style=" line-height: normal;"><p class="wb-stl-normal">You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the contact form on our website. We wish you a good day! You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the contact form on our website. We wish you a good day! You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the contact form on our website. We wish you a good day! You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the contact form on our website. We wish you a good day! You will find the latest information about...</p></div><div id="wb_element_instance23" class="wb_element wb_element_picture" title=""><img alt="gallery/big_ring" src="gallery_gen/5652020a1bdb066641b0172598a54c5b.png"></div><div id="wb_element_instance24" class="wb_element" style="width: 100%;">
-			<?php
-			global $show_comments;
-			if (isset($show_comments) && $show_comments) {
-				renderComments(2);
-				?>
-				<script type="text/javascript">
-					$(function() {
-						var block = $("#wb_element_instance24");
-						var comments = block.children(".wb_comments").eq(0);
-						var contentBlock = $("#wb_main");
-						contentBlock.height(contentBlock.height() + comments.height());
-					});
-				</script>
-				<?php
-			} else {
-				?>
-				<script type="text/javascript">
-					$(function() {
-						$("#wb_element_instance24").hide();
-					});
-				</script>
-				<?php
-			}
-			?>
-		</div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div>
-		<div class="vbox wb_container" id="wb_footer">
-			
-			<div class="wb_cont_inner" style="height: 144px;"><div id="wb_element_instance17" class="wb_element wb_text_element" style=" line-height: normal;"><p class="wb-stl-footer">© 2019 <a href="http://dignityforvolunteer.000webhostapp.com">dignityforvolunteer.000webhostapp.com</a></p></div><div id="wb_element_instance25" class="wb_element" style="text-align: center; width: 100%;"><div class="wb_footer"></div><script type="text/javascript">
-				$(function() {
-					var footer = $(".wb_footer");
-					var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
-					if (!html) {
-						footer.parent().remove();
-						footer = $("#wb_footer, #wb_footer .wb_cont_inner");
-						footer.css({height: ""});
-					}
-				});
-			</script></div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div><div class="wb_sbg"></div></div>{{hr_out}}</body>
+			<div class="wb_cont_inner">
+				<div id="wb_element_instance15" class="wb_element wb-menu">
+					<ul class="hmenu">
+						<li><a href="" target="_self">Home</a></li>
+						<li><a href="Event/" target="_self">Event</a></li>
+						<li class="active"><a href="About/" target="_self">About</a></li>
+						<li><a href="Login/" target="_self">Login</a></li>
+					</ul>
+					<div class="clearfix"></div>
+				</div>
+				<div id="wb_element_instance16" class="wb_element wb_element_picture" title="">
+					<a href="http://dignityforchildren.org/" target="1"><img alt="gallery/dignity_logo" src="gallery_gen/37c944c27b869908c211dea96575621f_190x60.png"></a>
+				</div>
+			</div>
+			<div class="wb_cont_outer"></div>
+			<div class="wb_cont_bg"></div>
+		</div>
+		<div class="vbox wb_container" id="wb_main">
+
+			<div class="wb_cont_inner">
+				<div id="wb_element_instance18" class="wb_element wb_text_element" style=" line-height: normal;">
+					<p class="wb-stl-normal">You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the contact form on our website. We wish you a good day! You will find the latest information about us on this page. Our company is constantly evolving and...</p>
+				</div>
+				<div id="wb_element_instance19" class="wb_element wb_text_element" style=" line-height: normal;">
+					<h2 class="wb-stl-heading2">You will find the latest information...<span style="background-color: transparent;">About our company here. You will find the...</span><span style="background-color: transparent;">Latest information about our company...</span><span style="background-color: transparent;">Here. You will find the latest...</span>Information about our...</h2></div>
+					<div id="wb_element_instance20" class="wb_element wb_element_shape">
+						<div class="wb_shp"></div>
+					</div>
+					<div id="wb_element_instance21" class="wb_element wb_text_element" style=" line-height: normal;">
+						<h1 class="wb-stl-heading1">About us</h1></div>
+						<div id="wb_element_instance22" class="wb_element wb_text_element" style=" line-height: normal;">
+							<p class="wb-stl-normal">You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the contact form on our website. We wish you a good day! You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the contact form on our website. We wish you a good day! You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the contact form on our website. We wish you a good day! You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the contact form on our website. We wish you a good day! You will find the latest information about...</p>
+						</div>
+						<div id="wb_element_instance23" class="wb_element wb_element_picture" title=""><img alt="gallery/big_ring" src="gallery_gen/5652020a1bdb066641b0172598a54c5b.png"></div>
+						<div id="wb_element_instance24" class="wb_element" style="width: 100%;">
+							<?php
+							global $show_comments;
+							if (isset($show_comments) && $show_comments) {
+								renderComments(2);
+								?>
+								<script type="text/javascript">
+									$(function() {
+										var block = $("#wb_element_instance24");
+										var comments = block.children(".wb_comments").eq(0);
+										var contentBlock = $("#wb_main");
+										contentBlock.height(contentBlock.height() + comments.height());
+									});
+								</script>
+								<?php
+							} else {
+								?>
+								<script type="text/javascript">
+									$(function() {
+										$("#wb_element_instance24").hide();
+									});
+								</script>
+								<?php
+							}
+							?>
+						</div>
+					</div>
+					<div class="wb_cont_outer"></div>
+					<div class="wb_cont_bg"></div>
+				</div>
+				<div class="vbox wb_container" id="wb_footer">
+
+					<div class="wb_cont_inner" style="height: 144px;">
+						<div id="wb_element_instance17" class="wb_element wb_text_element" style=" line-height: normal;">
+							<p class="wb-stl-footer">© 2019 <a href="http://dignityforvolunteer.000webhostapp.com">dignityforvolunteer.000webhostapp.com</a></p>
+						</div>
+						<div id="wb_element_instance25" class="wb_element" style="text-align: center; width: 100%;">
+							<div class="wb_footer"></div>
+							<script type="text/javascript">
+								$(function() {
+									var footer = $(".wb_footer");
+									var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
+									if (!html) {
+										footer.parent().remove();
+										footer = $("#wb_footer, #wb_footer .wb_cont_inner");
+										footer.css({
+											height: ""
+										});
+									}
+								});
+							</script>
+						</div>
+					</div>
+					<div class="wb_cont_outer"></div>
+					<div class="wb_cont_bg"></div>
+				</div>
+				<div class="wb_sbg"></div>
+			</div>{{hr_out}}</body>
+
 			</html>
--- public_html/view/3.php
+++ public_html/view/3.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<title>Login</title>
 	<base href="{{base_url}}" />
-			<meta name="viewport" content="width=1200" />
-		<meta name="description" content="" />
+	<meta name="viewport" content="width=1200" />
+	<meta name="description" content="" />
 	<meta name="keywords" content="" />
 	<!-- Facebook Open Graph -->
 	<meta property="og:title" content="Login" />
@@ -14,7 +15,7 @@
 	<meta property="og:type" content="article" />
 	<meta property="og:url" content="{{curr_url}}" />
 	<!-- Facebook Open Graph end -->
-		
+
 	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 	<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
 	<script src="js/bootstrap.min.js" type="text/javascript"></script>
@@ -26,86 +27,189 @@
 	<link href="css/3.css?ts=1556705653" rel="stylesheet" type="text/css" />
 	<ga-code/>
 	<script type="text/javascript">
-	window.useTrailingSlashes = true;
-</script>
-	
-	<link href="css/flag-icon-css/css/flag-icon.min.css" rel="stylesheet" type="text/css" />	
-	<!--[if lt IE 9]>
+		window.useTrailingSlashes = true;
+	</script>
+
+	<link href="css/flag-icon-css/css/flag-icon.min.css" rel="stylesheet" type="text/css" />
+    <!--[if lt IE 9]>
 	<script src="js/html5shiv.min.js"></script>
-	<![endif]-->
+<![endif]-->
 
-	</head>
+</head>
 
+<body>
+	<div class="root">
+		<div class="vbox wb_container" id="wb_header">
 
-<body><div class="root"><div class="vbox wb_container" id="wb_header">
-	
-<div class="wb_cont_inner"><div id="wb_element_instance26" class="wb_element wb-menu"><ul class="hmenu"><li><a href="" target="_self">Home</a></li><li><a href="Event/" target="_self">Event</a></li><li><a href="About/" target="_self">About</a></li><li class="active"><a href="Login/" target="_self">Login</a></li></ul><div class="clearfix"></div></div><div id="wb_element_instance27" class="wb_element wb_element_picture" title=""><a href="http://dignityforchildren.org/" target="1"><img alt="gallery/dignity_logo" src="gallery_gen/37c944c27b869908c211dea96575621f_190x60.png"></a></div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div>
-<div class="vbox wb_container" id="wb_main">
-	
-<div class="wb_cont_inner"><div id="wb_element_instance29" class="wb_element wb_text_element" style=" line-height: normal;"><h1 class="wb-stl-heading1">Become Volunteer ...</h1></div><div id="wb_element_instance30" class="wb_element wb_text_element" style=" line-height: normal;"><p class="wb-stl-normal"><span dir="ltr" style="direction: ltr;">+1 212 736 3100</span></p>
-<p class="wb-stl-normal">Empire State Building</p>
-<p class="wb-stl-normal">350 5th Ave</p>
-<p class="wb-stl-normal">New York</p>
-<p class="wb-stl-normal">NY 10118</p>
-<p class="wb-stl-normal">USA</p></div><div id="wb_element_instance31" class="wb_element wb_text_element" style=" line-height: normal;"><h2 class="wb-stl-heading2">This text should be replaced with information about you and your business This text should be replaced with information about you and your business This text should be...</h2></div><div id="wb_element_instance32" class="wb_element"><form class="wb_form" method="post" enctype="multipart/form-data"><input type="hidden" name="wb_form_id" value="a9e12055"><textarea name="message" rows="3" cols="20" class="hpc"></textarea><table><tr><th>Name&nbsp;&nbsp;</th><td><input type="hidden" name="wb_input_0" value="Name"><input class="form-control form-field" type="text" value="" name="wb_input_0" required="required"></td></tr><tr><th>E-mail&nbsp;&nbsp;</th><td><input type="hidden" name="wb_input_1" value="E-mail"><input class="form-control form-field" type="text" value="" name="wb_input_1" required="required"></td></tr><tr class="area-row"><th>Message&nbsp;&nbsp;</th><td><input type="hidden" name="wb_input_2" value="Message"><textarea class="form-control form-field form-area-field" rows="3" cols="20" name="wb_input_2" required="required"></textarea></td></tr><tr class="form-footer"><td colspan="2"><button type="submit" class="btn btn-default">Submit</button></td></tr></table></form><script type="text/javascript">
-			<?php $wb_form_id = popSessionOrGlobalVar("wb_form_id"); if ($wb_form_id == "a9e12055") { ?>
-				var formValues = <?php echo json_encode(popSessionOrGlobalVar("post")); ?>;
-				var formErrors = <?php echo json_encode(popSessionOrGlobalVar("formErrors")); ?>;
-				wb_form_validateForm("a9e12055", formValues, formErrors);
-				<?php $wb_form_send_success = popSessionOrGlobalVar("wb_form_send_success");
-				if (($wb_form_send_state = popSessionOrGlobalVar("wb_form_send_state"))) { ?>
-					var prompt = $("<div>")
-						.addClass("alert alert-<?php echo $wb_form_send_success ? "success" : "danger"; ?>")
-						.css({ position: "fixed", opacity: 0, right: "-50px", top: "10px", zIndex: 10000, fontSize: "24px",
-							   padding: "30px 50px", lineHeight: "24px", maxWidth: "748px" })
-						.append("<?php echo str_replace('"', '\"', $wb_form_send_state); ?>")
-						.prepend($("<button>").addClass("close")
-							.css({ marginRight: "-40px", marginTop: "-24px" })
-							.html("&nbsp;&times;")
-							.on("click", function() { $(this).parent().remove(); })
-						)
-					.appendTo("body");
-					setTimeout(function() { prompt.animate({ opacity: 1, right: "10px" }, 250); }, 250);
-					<?php $wb_form_send_success = false; $wb_form_send_state = null; ?>
-				<?php } ?>
-			<?php } ?>
-			</script></div><div id="wb_element_instance33" class="wb_element" style="width: 100%;">
-			<?php
-				global $show_comments;
-				if (isset($show_comments) && $show_comments) {
-					renderComments(3);
-			?>
-			<script type="text/javascript">
-				$(function() {
-					var block = $("#wb_element_instance33");
-					var comments = block.children(".wb_comments").eq(0);
-					var contentBlock = $("#wb_main");
-					contentBlock.height(contentBlock.height() + comments.height());
-				});
-			</script>
-			<?php
-				} else {
-			?>
-			<script type="text/javascript">
-				$(function() {
-					$("#wb_element_instance33").hide();
-				});
-			</script>
-			<?php
-				}
-			?>
-			</div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div>
-<div class="vbox wb_container" id="wb_footer">
-	
-<div class="wb_cont_inner" style="height: 144px;"><div id="wb_element_instance28" class="wb_element wb_text_element" style=" line-height: normal;"><p class="wb-stl-footer">© 2019 <a href="http://dignityforvolunteer.000webhostapp.com">dignityforvolunteer.000webhostapp.com</a></p></div><div id="wb_element_instance34" class="wb_element" style="text-align: center; width: 100%;"><div class="wb_footer"></div><script type="text/javascript">
-			$(function() {
-				var footer = $(".wb_footer");
-				var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
-				if (!html) {
-					footer.parent().remove();
-					footer = $("#wb_footer, #wb_footer .wb_cont_inner");
-					footer.css({height: ""});
-				}
-			});
-			</script></div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div><div class="wb_sbg"></div></div>{{hr_out}}</body>
-</html>
+			<div class="wb_cont_inner">
+				<div id="wb_element_instance26" class="wb_element wb-menu">
+					<ul class="hmenu">
+						<li><a href="" target="_self">Home</a></li>
+						<li><a href="Event/" target="_self">Event</a></li>
+						<li><a href="About/" target="_self">About</a></li>
+						<li class="active"><a href="Login/" target="_self">Login</a></li>
+					</ul>
+					<div class="clearfix"></div>
+				</div>
+				<div id="wb_element_instance27" class="wb_element wb_element_picture" title="">
+					<a href="http://dignityforchildren.org/" target="1"><img alt="gallery/dignity_logo" src="gallery_gen/37c944c27b869908c211dea96575621f_190x60.png"></a>
+				</div>
+			</div>
+			<div class="wb_cont_outer"></div>
+			<div class="wb_cont_bg"></div>
+		</div>
+		<div class="vbox wb_container" id="wb_main">
+
+			<div class="wb_cont_inner">
+				<div id="wb_element_instance29" class="wb_element wb_text_element" style=" line-height: normal;">
+					<h1 class="wb-stl-heading1">Become Volunteer ...</h1></div>
+					<div id="wb_element_instance30" class="wb_element wb_text_element" style=" line-height: normal;">
+						<p class="wb-stl-normal"><span dir="ltr" style="direction: ltr;">+1 212 736 3100</span></p>
+						<p class="wb-stl-normal">Empire State Building</p>
+						<p class="wb-stl-normal">350 5th Ave</p>
+						<p class="wb-stl-normal">New York</p>
+						<p class="wb-stl-normal">NY 10118</p>
+						<p class="wb-stl-normal">USA</p>
+					</div>
+					<div id="wb_element_instance31" class="wb_element wb_text_element" style=" line-height: normal;">
+						<h2 class="wb-stl-heading2">This text should be replaced with information about you and your business This text should be replaced with information about you and your business This text should be...</h2></div>
+						<div id="wb_element_instance32" class="wb_element">
+							<form class="wb_form" method="post" enctype="multipart/form-data">
+								<input type="hidden" name="wb_form_id" value="a9e12055">
+								<textarea name="message" rows="3" cols="20" class="hpc"></textarea>
+								<table>
+									<tr>
+										<th>Name&nbsp;&nbsp;</th>
+										<td>
+											<input type="hidden" name="wb_input_0" value="Name">
+											<input class="form-control form-field" type="text" value="" name="wb_input_0" required="required">
+										</td>
+									</tr>
+									<tr>
+										<th>E-mail&nbsp;&nbsp;</th>
+										<td>
+											<input type="hidden" name="wb_input_1" value="E-mail">
+											<input class="form-control form-field" type="text" value="" name="wb_input_1" required="required">
+										</td>
+									</tr>
+									<tr class="area-row">
+										<th>Message&nbsp;&nbsp;</th>
+										<td>
+											<input type="hidden" name="wb_input_2" value="Message">
+											<textarea class="form-control form-field form-area-field" rows="3" cols="20" name="wb_input_2" required="required"></textarea>
+										</td>
+									</tr>
+									<tr class="form-footer">
+										<td colspan="2">
+											<button type="submit" class="btn btn-default">Submit</button>
+										</td>
+									</tr>
+								</table>
+							</form>
+							<script type="text/javascript">
+								<?php $wb_form_id = popSessionOrGlobalVar("wb_form_id"); if ($wb_form_id == "a9e12055") { ?>
+									var formValues = <?php echo json_encode(popSessionOrGlobalVar("post")); ?>;
+									var formErrors = <?php echo json_encode(popSessionOrGlobalVar("formErrors")); ?>;
+									wb_form_validateForm("a9e12055", formValues, formErrors);
+									<?php $wb_form_send_success = popSessionOrGlobalVar("wb_form_send_success");
+									if (($wb_form_send_state = popSessionOrGlobalVar("wb_form_send_state"))) { ?>
+										var prompt = $("<div>")
+										.addClass("alert alert-<?php echo $wb_form_send_success ? "
+											success " : "
+											danger "; ?>")
+										.css({
+											position: "fixed",
+											opacity: 0,
+											right: "-50px",
+											top: "10px",
+											zIndex: 10000,
+											fontSize: "24px",
+											padding: "30px 50px",
+											lineHeight: "24px",
+											maxWidth: "748px"
+										})
+										.append("<?php echo str_replace('"
+											', '\
+											"', $wb_form_send_state); ?>")
+										.prepend($("<button>").addClass("close")
+											.css({
+												marginRight: "-40px",
+												marginTop: "-24px"
+											})
+											.html("&nbsp;&times;")
+											.on("click", function() {
+												$(this).parent().remove();
+											})
+											)
+										.appendTo("body");
+										setTimeout(function() {
+											prompt.animate({
+												opacity: 1,
+												right: "10px"
+											}, 250);
+										}, 250);
+										<?php $wb_form_send_success = false; $wb_form_send_state = null; ?>
+									<?php } ?>
+								<?php } ?>
+							</script>
+						</div>
+						<div id="wb_element_instance33" class="wb_element" style="width: 100%;">
+							<?php
+							global $show_comments;
+							if (isset($show_comments) && $show_comments) {
+								renderComments(3);
+								?>
+								<script type="text/javascript">
+									$(function() {
+										var block = $("#wb_element_instance33");
+										var comments = block.children(".wb_comments").eq(0);
+										var contentBlock = $("#wb_main");
+										contentBlock.height(contentBlock.height() + comments.height());
+									});
+								</script>
+								<?php
+							} else {
+								?>
+								<script type="text/javascript">
+									$(function() {
+										$("#wb_element_instance33").hide();
+									});
+								</script>
+								<?php
+							}
+							?>
+						</div>
+					</div>
+					<div class="wb_cont_outer"></div>
+					<div class="wb_cont_bg"></div>
+				</div>
+				<div class="vbox wb_container" id="wb_footer">
+
+					<div class="wb_cont_inner" style="height: 144px;">
+						<div id="wb_element_instance28" class="wb_element wb_text_element" style=" line-height: normal;">
+							<p class="wb-stl-footer">© 2019 <a href="http://dignityforvolunteer.000webhostapp.com">dignityforvolunteer.000webhostapp.com</a></p>
+						</div>
+						<div id="wb_element_instance34" class="wb_element" style="text-align: center; width: 100%;">
+							<div class="wb_footer"></div>
+							<script type="text/javascript">
+								$(function() {
+									var footer = $(".wb_footer");
+									var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
+									if (!html) {
+										footer.parent().remove();
+										footer = $("#wb_footer, #wb_footer .wb_cont_inner");
+										footer.css({
+											height: ""
+										});
+									}
+								});
+							</script>
+						</div>
+					</div>
+					<div class="wb_cont_outer"></div>
+					<div class="wb_cont_bg"></div>
+				</div>
+				<div class="wb_sbg"></div>
+			</div>{{hr_out}}</body>
+
+			</html>
