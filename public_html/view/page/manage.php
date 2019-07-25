4cc75c26d078230bfe4e992f52727808c987d6b9
neiamenase <neiamenase@gmail.com>
2019-07-25 22:03:16 +0800

    update management

--- public_html/view/page/manage.php
+++ public_html/view/page/manage.php
@@ -29,11 +29,12 @@
 	<script src="js/ocLazyLoad.min.js" type="text/javascript"></script>
 	<script src="../controller/ng-common.js" type="text/javascript"></script>
 	<script src="../controller/loginController.js"></script>
-<script src="../controller/slideController.js" type="text/javascript"></script>
+	<script src="../controller/slideController.js" type="text/javascript"></script>
 
 	<link href="css/font-awesome/font-awesome.min.css?v=4.7.0" rel="stylesheet" type="text/css" />
 	<link href="css/site.css?v=20190117142750" rel="stylesheet" type="text/css" />
 	<link href="css/common.css?ts=1556705653" rel="stylesheet" type="text/css" />
+	<link href="css/manage.css?ts=1556705653" rel="stylesheet" type="text/css" />
 	<link rel="stylesheet" href="css/loginForm.css">
 	<!-- <link href="css/event.css?ts=1556705653" rel="stylesheet" type="text/css" /> -->
 	<ga-code/>
@@ -47,38 +48,38 @@
 	<div class="loader" ng-show="loading"> </div>
 	<commonheader></commonheader>
 	<div id="container">
-  <ul id="slides">
-    <li class="slide active">
-      <div class="slide-img"><img src="https://wallpapercave.com/wp/wp2438853.jpg"/></div>
-      <h1 class="title"><span class="title-text">11111</span></h1>
-    </li>
-    <li class="slide">
-      <div class="slide-img"><img src="https://wallpapercave.com/wp/wp3285738.jpg"/></div>
-      <h1 class="title"><span class="title-text">22222</span></h1>
-    </li>
-    <li class="slide">
-      <div class="slide-img"><img src="https://wallpapercave.com/wp/wp3285747.jpg"/></div>
-      <h1 class="title"><span class="title-text">33333</span></h1>
-    </li>
-    <li class="slide">
-      <div class="slide-img"><img src="https://wallpapercave.com/wp/wp2686919.jpg"/></div>
-     <h1 class="title"><span class="title-text">44444</span></h1>
-    </li>
-    <li class="slide">
-      <div class="slide-img"><img src="https://en.bcdn.biz/Images/2016/10/28/e68fb895-9ba1-4400-badf-d6741c1bb197.jpg"/></div>
-      <h1 class="title"><span class="title-text">55555</span></h1>
-    </li>
-  </ul>
-  <ul id="slide-select">
-    <li class="btn prev"><</li>
-    <li class="selector"></li>
-    <li class="selector"></li>
-    <li class="selector"></li>
-    <li class="selector"></li>
-    <li class="selector"></li>
-    <li class="btn next">></li>
-  </ul>
-</div>
+		<ul id="slides">
+			<li class="slide active">
+				<div class="slide-img"><img src="https://wallpapercave.com/wp/wp2438853.jpg"/></div>
+				<h1 class="title"><span class="title-text">11111</span></h1>
+			</li>
+			<li class="slide">
+				<div class="slide-img"><img src="https://wallpapercave.com/wp/wp3285738.jpg"/></div>
+				<h1 class="title"><span class="title-text">22222</span></h1>
+			</li>
+			<li class="slide">
+				<div class="slide-img"><img src="https://wallpapercave.com/wp/wp3285747.jpg"/></div>
+				<h1 class="title"><span class="title-text">33333</span></h1>
+			</li>
+			<li class="slide">
+				<div class="slide-img"><img src="https://wallpapercave.com/wp/wp2686919.jpg"/></div>
+				<h1 class="title"><span class="title-text">44444</span></h1>
+			</li>
+			<li class="slide">
+				<div class="slide-img"><img src="https://en.bcdn.biz/Images/2016/10/28/e68fb895-9ba1-4400-badf-d6741c1bb197.jpg"/></div>
+				<h1 class="title"><span class="title-text">55555</span></h1>
+			</li>
+		</ul>
+		<ul id="slide-select">
+			<li class="btn prev"><</li>
+			<li class="selector"></li>
+			<li class="selector"></li>
+			<li class="selector"></li>
+			<li class="selector"></li>
+			<li class="selector"></li>
+			<li class="btn next">></li>
+		</ul>
+	</div>
 
 	<div class="textbody below_slide">
 
@@ -87,16 +88,16 @@
 
 
 
-			<h1>Past Event</h1>
+			<h1>Events</h1>
 			<table st-table="pastEvents" class="table table-striped">
 				<thead>
 					<tr>
-						<th>Event Name</th>
+						<th>Name</th>
 						<th>Period</th>
 						<th>Venue</th>
 						<th>Application Deadline</th>
 						<th>Quota</th>
-						<th>Action</th>
+						<th></th>
 					</tr>
 				</thead>
 				<tbody>
@@ -112,72 +113,150 @@
 			</table>
 		</div>
 		<div>
-			<h1>Create Event</h1>
-			<label >Id</label> <br>
-			{{eventDetail.id}}<br>
-			<label >Name</label><br>
-			<input id="name" type="text" ng-model="eventDetail.name"/><br>
-			<label >From </label><br>
-			<input id="fromDate" type="date" ng-model="eventDetail.fromDate"/>  
-			<input id="fromTime" type="time" ng-model="eventDetail.fromDate"/><br>
-			<input id="fromDate1" type="text" ng-model="eventDetail.fromDate"/><br>
-			<label >To</label><br>
-			<input id="toDate" type="date" ng-model="eventDetail.toDate"/>
-			<input id="toTime" type="time" ng-model="eventDetail.toDate"/><br>
-			<input id="toDate1" type="text" ng-model="eventDetail.toDate"/><br>
-			<label >Venue</label><br>
-			<input id="venue" type="text" ng-model="eventDetail.venue"/><br>
-			<label >Location</label><br>
-			<input id="location" type="text" ng-model="eventDetail.location"/><br>
-			<label >Contact Person Name</label><br>
-			<input id="contactName" type="text" ng-model="eventDetail.contactName"/><br>
-			<label >Contact Person Email</label><br>
-			<input id="loginEmail" type="text" ng-model="eventDetail.contactEmail"/><br>
-			<label >Application Deadline</label><br>
-			<input id="applictionDeadline" type="date" ng-model="eventDetail.applicationDeadline"/><br>
-			<input id="applictionDeadline1" type="text" ng-model="eventDetail.applicationDeadline"/><br>
-			<label >Quota</label><br>
-			<input id="quota" type="number" ng-model="eventDetail.quota"/><br>
-			<label>Active</label><br>
-			<select ng-model="eventDetail.active">
-				<option value="1">Active
-				<option value="0">Cancelled
-			</select><br>
-					<label >Registered Number:  {{eventDetail.registered}}</label><br>
-					<label >Remaing Quota:  {{eventDetail.quota - eventDetail.registered}}<br></label>
-			<br><br><br>
-
-				<button ng-click="postEvent()">Save</button>
-				<!-- <button ng-click="getRegisteredList()">Get Registered List</button> -->
-				</div>
-
-					<div>
-			<h1>Registered Volunteer</h1>
-			<table st-table="registered" class="table table-striped">
-				<thead>
-					<tr>
-						<th style="min-width: 5vw;">No.</th>
-						<th style="min-width: 10vw;">Volunteer id</th>
-						<th style="min-width: 20vw;">Name</th>
-						<th style="min-width: 25vw;">Email</th>
-						<th style="min-width: 20vw;">Registered Date</th>
-						<th style="min-width: 10vw;">Active</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr ng-repeat="row in registeredList">
-						<td>{{$index + 1}}</td>
-						<td>{{row.volId}}</td>
-						<td>{{row.name}}</td>
-						<td>{{row.email}}</td>
-						<td>{{row.createDate}}</td>
-						<td>{{row.active == "0"? 'inactive':'active'}}</td>
+			<h1>{{action}}}</h1>
+			<table>
+				<tr>
+					<td class="tdheader">
+						<label>Id</label> 
+					</td>
+					<td class="tdcontent">
+						{{eventDetail.id}}
+					</td>
+					<td class="tdheader">
+						<label >Name</label>
+					</td>
+					<td class="tdcontent">
+						<input id="name" type="text" ng-model="eventDetail.name"/>
 						
-					</tr>
-				</tbody>
-			</table>
-			</div>
-		</div>
-		<commonfooter></commonfooter></body>
+					</td>
+				</tr>
+				<tr>
 
-	</html>
+					<td class="tdheader">
+						<label >From </label>
+						
+					</td>
+					<td class="tdcontent">
+						<input id="fromDate" type="date" ng-model="eventDetail.fromDate"/>  
+						<input id="fromTime" type="time" ng-model="eventDetail.fromDate"/><br>
+						<input id="fromDate1" type="text" ng-model="eventDetail.fromDate"/><br>
+						
+					</td>
+					<td class="tdheader">
+						<label >To</label>
+						
+					</td>
+					<td class="tdcontent">
+						<input id="toDate" type="date" ng-model="eventDetail.toDate"/>
+						<input id="toTime" type="time" ng-model="eventDetail.toDate"/><br>
+						<input id="toDate1" type="text" ng-model="eventDetail.toDate"/><br>
+					</td>
+				</tr>
+				<tr>
+					<td class="tdheader">
+						<label >Venue</label>
+					</td>
+					<td class="tdcontent">
+						<input id="venue" type="text" ng-model="eventDetail.venue"/>
+					</td>
+					<td class="tdheader">
+						<label >Location</label>
+					</td>
+					<td class="tdcontent">
+						<input id="location" type="text" ng-model="eventDetail.location"/>
+					</td>
+				</tr>
+				<tr>
+					<td class="tdheader">
+						<label >Contact Person Name</label>
+					</td>
+					<td class="tdcontent">
+						<input id="contactName" type="text" ng-model="eventDetail.contactName"/>
+					</td>
+					<td class="tdheader">
+						<label >Contact Person Email</label>
+					</td>
+					<td class="tdcontent">
+						<input id="loginEmail" type="text" ng-model="eventDetail.contactEmail"/>
+					</td>
+				</tr>
+			</td>
+			<tr>
+				<td class="tdheader">
+					<label >Application Deadline</label>
+				</td>
+				<td class="tdcontent">
+					<input id="applictionDeadline" type="date" ng-model="eventDetail.applicationDeadline"/><br>
+					<input id="applictionDeadline1" type="text" ng-model="eventDetail.applicationDeadline"/>
+				</td>
+				<td class="tdheader">
+					<label >Quota</label>
+				</td>
+				<td class="tdcontent">
+					<input id="quota" type="number" ng-model="eventDetail.quota"/>
+				</td>
+			</tr>
+			<tr>
+				<td class="tdheader">						
+					<label>Active</label>
+				</td>
+				<td class="tdcontent">
+					<select ng-model="eventDetail.active">
+						<option value="1">Active</option>
+						<option value="0">Cancelled</option>
+					</select>
+				</td>
+				<td class="tdheader">
+					<label >Registered Number: </label> 
+				</td>
+				<td class="tdcontent">
+					{{eventDetail.registered}}
+				</td>
+
+			</tr>
+			<tr>
+				<td class="tdheader">
+					<label >Remaing Quota: </label>
+				</td>
+				<td class="tdcontent"> 
+					{{eventDetail.quota - eventDetail.registered}}
+				</td>
+			</tr>
+		</table>
+
+
+
+		<button ng-click="postEvent()">Save</button>
+		<!-- <button ng-click="getRegisteredList()">Get Registered List</button> -->
+	</div>
+
+	<div>
+		<h1>Registered Volunteer</h1>
+		<table st-table="registered" class="table table-striped">
+			<thead>
+				<tr>
+					<th style="min-width: 5vw;">No.</th>
+					<th style="min-width: 10vw;">Volunteer id</th>
+					<th style="min-width: 20vw;">Name</th>
+					<th style="min-width: 25vw;">Email</th>
+					<th style="min-width: 20vw;">Registered Date</th>
+					<th style="min-width: 10vw;">Active</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr ng-repeat="row in registeredList">
+					<td>{{$index + 1}}</td>
+					<td>{{row.volId}}</td>
+					<td>{{row.name}}</td>
+					<td>{{row.email}}</td>
+					<td>{{row.createDate}}</td>
+					<td>{{row.active == "0"? 'inactive':'active'}}</td>
+
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
+<commonfooter></commonfooter></body>
+
+</html>
